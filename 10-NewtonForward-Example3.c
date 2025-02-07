a455f959bab51375620e5539a1bb1b2af1b5669d
Prof. (Dr) Hiren Patel <hbpatel1976@gmail.com>
2025-02-07 15:48:50 +0530

    Add files via upload

---
+++ 10-NewtonForward-Example3.c
@@ -0,0 +1,55 @@
+/* Newton's Forward Interpolation Formula */
+#include <stdio.h>
+#define size 7
+void main()
+{
+	float x[size]={0,1,2,3,4,5,6}, findx=2.5;
+	float y[size]={0, 0.8415, 0.9093, 0.1411, -0.7568, -0.9589, -0.2794}, findy, deltay[size][size];
+	float h, u;
+	
+	int i, j;
+	float tempu, fact;
+	
+	h=x[1]-x[0];
+	
+	/* Equal Distance Between x values */
+	for(i=2; i<size; ++i)
+	{if((int)(x[i]-x[i-1]) != (int)(h))
+		{
+			printf("The values of x are not at equal distnace and hence, Newton Forward Interpolation is not possible");
+			return;
+		}
+	}
+	
+	u = (findx - x[0])/h;
+	printf("h = %8.5f, u=%8.5f\n",h,u);
+	
+	/* Find the value of Delta Y*/
+	for(i=0; i<size; ++i)deltay[i][0]=y[i];
+	
+	for(j=1; j<size; ++j)
+	{
+		for(i=0; i<size-j; ++i)
+		{
+			deltay[i][j] = deltay[i+1][j-1] - deltay[i][j-1];
+		}
+	}
+	for(i=0; i<size; ++i)
+	{
+		for(j=0; j<size-i; ++j)
+		{
+			printf("%8.5f\t",deltay[i][j]);
+		}
+		printf("\n");
+	}
+	findy = y[0];
+	tempu = 1;
+	fact = 1;
+	for(i=0; i<size-1; ++i)
+	{
+		tempu = tempu * (u-i);	
+		fact = fact * (i+1);
+		findy = findy + (tempu/fact)*deltay[0][i+1];
+	}
+	printf("Answer  = %8.5f",findy);
+}
